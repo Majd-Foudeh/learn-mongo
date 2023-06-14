2a1cf53d1c24c5de2762edc94c53551079885fe6
MajdFoudeh <majdfoude@gmail.com>
2023-06-14 16:02:23 +0300

    finish

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,4 +1,12 @@
 const user = require('../models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+function jwttoken({ first_Name, last_Name, user_email }) {
+    const userdata = { first_Name, last_Name, user_email }
+    const token = jwt.sign(userdata, process.env.ACCESS_TOKEN_SECRET)
+    return token;
+}
 
 const allusers = (req, res) => {
     user.find()
@@ -12,4 +20,58 @@ const allusers = (req, res) => {
 
 }
 
-module.exports = { allusers }
+
+const adduser = async (req, res) => {
+    try {
+
+        const { fname, lname, email, password, type } = req.body
+        const token = jwttoken({
+            first_Name: fname,
+            last_Name: lname,
+            user_email: email,
+        })
+
+        const isuserexist = await user.findOne({ user_email: email })
+        if (isuserexist) {
+            return res.json({ error: "this email is allready exists" })
+        }
+        const newaccount = new user({
+            first_Name: fname,
+            last_Name: lname,
+            user_email: email,
+            user_password: password,
+            user_type: type,
+            user_token: token,
+        })
+        const newuser = await newaccount.save()
+        res.json({ message: "Signup successfuly" })
+    } catch (error) {
+        console.error('error in signup', error);
+        res.json({ message: 'internal server error' })
+    }
+}
+
+const login = async (req, res) => {
+
+    try {
+        // check if the user exists
+        const { email, password } = req.body
+        const isuser = await user.findOne({ user_email: email })
+        if (!isuser) {
+            return res.json({ error: "email is not found" })
+        }
+        // check if the password is correct 
+        const checkpassword = await bcrypt.compare(password, isuser.user_password)
+        if (!checkpassword) {
+            return res.json({ error: "incorrect password" })
+        }
+
+
+        res.json(isuser.user_token)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+module.exports = { allusers, adduser, login }
--- routes/userrouter.js
+++ routes/userrouter.js
@@ -4,7 +4,8 @@ const usercontroller = require('../controllers/usercontroller')
 
 
 router.get('/users', usercontroller.allusers)
-// router.post('/newusers', usercontroller.newuser)
+router.post('/adduser', usercontroller.adduser)
+router.post('/login', usercontroller.login)
 // router.getone('/oneuser', usercontroller.oneuser)
 // router.put('/updateusers', usercontroller.updateusers)
 // router.delete('/deleteusers', usercontroller.deleteusers)
